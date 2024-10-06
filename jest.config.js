602000b6de2603418a43ff3ecf8d9c37ea9bb73e
lazaryan <lazaryan99@gmail.com>
2024-10-06 11:24:32 +0300

    (feat): init unit tests

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// For a detailed explanation regarding each configuration property, visit:
+// * https://jestjs.io/docs/en/configuration.html
+// * https://kulshekhar.github.io/ts-jest/user/config/
+
+export default {
+  preset: 'ts-jest',
+  roots: ['src'],
+  collectCoverageFrom: [
+    'src/**/{!(_public|index|testUtils),}.{ts,tsx}',
+    '!**/test/**',
+  ],
+};
---
+++ src/transport.test.ts
@@ -0,0 +1,33 @@
+import { createTransport } from './transport';
+
+describe('smoke tests', () => {
+  it('init transport', () => {
+    expect(createTransport.bind(null)).not.toThrow();
+  });
+  it('destroy transport', () => {
+    const transport = createTransport();
+    expect(transport.destroy.bind(transport)).not.toThrow();
+  });
+  it('subscribe', () => {
+    const transport = createTransport();
+    expect(transport.on.bind(transport, '*', () => {})).not.toThrow();
+    expect(transport.destroy.bind(transport)).not.toThrow();
+  });
+  it('unsubscribe', () => {
+    const transport = createTransport();
+    const action = () => {};
+    transport.on('*', action);
+    expect(transport.off.bind(transport, '*', action)).not.toThrow();
+    expect(transport.destroy.bind(transport)).not.toThrow();
+  });
+  it('send', async () => {
+    const transport = createTransport<{ '': undefined }>();
+    expect(await transport.send.bind(transport, '')).not.toThrow();
+    transport.on('*', () => {});
+    expect(await transport.send.bind(transport, '')).not.toThrow();
+  });
+  it('asReadonly', () => {
+    const transport = createTransport();
+    expect(transport.asReadonly.bind(transport)).not.toThrow();
+  });
+});
