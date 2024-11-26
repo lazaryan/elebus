9b8776d601a6f3c794e4694b81d98c1216e1b811
lazaryan <lazaryan99@gmail.com>
2024-11-27 00:53:58 +0300

    feat: benchmark root readonly

--- benchmarks/index.ts
+++ benchmarks/index.ts
@@ -15,10 +15,15 @@ main();
 
 async function main(): Promise<void> {
   const benchRootElebus = await benchRootTransport();
+  const benchRootReadonlyElebus = await benchRootReadonlyTransport();
+
   const benchNrgy = await benchSignal();
 
   console.log(`======= ${benchRootElebus.name} =======`);
   console.table(benchRootElebus.table());
+
+  console.log(`======= ${benchRootReadonlyElebus.name} =======`);
+  console.table(benchRootReadonlyElebus.table());
 
   console.log(`======= ${benchNrgy.name} =======`);
   console.table(benchNrgy.table());
@@ -48,6 +53,48 @@ async function benchRootTransport(): Promise<Bench> {
   bench.add('sync send event', () => transportSync.send('event', undefined));
 
   await bench.run();
+
+  transportSync.destroy();
+  transportAsync.destroy();
+  subscribeTestTransport.destroy();
+
+  return bench;
+}
+
+async function benchRootReadonlyTransport(): Promise<Bench> {
+  const bench = new Bench({
+    time: BENCH_TIME,
+    name: 'elebus root readonly node',
+  });
+
+  const transportSync = createTransport<{ event: undefined, unknown_event: number }>({ sync: true });
+  const transportAsync = createTransport<{ event: undefined, unknown_event: number }>();
+
+  const transportSyncReadonly = transportSync.asReadonly();
+  const transportAsyncReadonly = transportSync.asReadonly();
+  bench.add('create readonly node', () => transportSync.asReadonly());
+
+  const subscribeTestTransport = createTransport<{ event: undefined, unknown_event: number }>();
+  const subscribeTestTransportReadonly = subscribeTestTransport.asReadonly();
+
+  bench.add('subscribe to root node', () => subscribeTestTransportReadonly.on('event', () => factorial(FACTORIAL_TEST_SIZE)));
+  for (let i = 0; i <= SUBSCRIBERS_COUNT; i++) {
+    transportSyncReadonly.on('event', () => factorial(FACTORIAL_TEST_SIZE));
+    transportAsyncReadonly.on('event', () => factorial(FACTORIAL_TEST_SIZE));
+  }
+
+  bench.add('async sending events that are not subscribed to', () => transportAsync.send('unknown_event', 123));
+  bench.add('sync sending events that are not subscribed to', () => transportSync.send('unknown_event', 123));
+
+  bench.add('async send event', () => transportAsync.send('event'));
+  bench.add('sync send event', () => transportSync.send('event', undefined));
+
+  await bench.run();
+
+  transportSync.destroy();
+  transportAsync.destroy();
+  subscribeTestTransport.destroy();
+  
   return bench;
 }
 
